2fa1a8ac8825c792f4b5403aa2be2e53e7e97979
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-19 14:22:41 +0200

    GRP-103: Allow using flask shell with context

--- groupsapi.py
+++ groupsapi.py
@@ -1,4 +1,10 @@
 from app import create_app
-
+from models import User, Group, Event
+from app.db import Session
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'Session': Session, 'User': User, 'Group': Group, 'Event': Event}
