5f172db924be641fe0e086f7b091a6dff0ba8ff4
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-24 16:36:23 +0200

    GRP-104: Abstract first_name and last_name in restx_models

--- app/api/users/restx_models.py
+++ app/api/users/restx_models.py
@@ -2,22 +2,21 @@ from flask_restx import fields
 
 from app.api.users.namespace import api_users
 
-user_create = api_users.model('UserCreate', {
+user_names = api_users.model('UserNames', {
     'first_name': fields.String,
     'last_name': fields.String,
+})
+
+user_create = api_users.inherit('UserCreate', user_names, {
     'password': fields.String,
     'email': fields.String,
 })
 
 
-user_created = api_users.model('UserCreated', {
+user_created = api_users.inherit('UserCreated', user_names, {
     'id': fields.Integer,
-    'first_name': fields.String,
-    'last_name': fields.String,
     'email': fields.String,
 })
 
-user_patch = api_users.model('UserPatch', {
-    'first_name': fields.String,
-    'last_name': fields.String,
+user_patch = api_users.inherit('UserPatch', user_names, {
 })
