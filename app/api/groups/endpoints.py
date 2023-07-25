4465efb3b313e28eea016cba0eb473e22945261b
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-25 15:05:34 +0200

    GRP-106: Create group

--- app/api/groups/endpoints.py
+++ app/api/groups/endpoints.py
@@ -6,7 +6,7 @@ from app.api.groups.marshmellow_schemas import GroupCreateSchema
 from app.api.groups.namespace import api_groups
 from app.api.groups.restx_models import group_create, group_created
 from app.validation import validate_schema
-from models import User
+from models import User, Group
 
 
 @api_groups.route("/")
@@ -16,7 +16,21 @@ class Groups(Resource):
     @api_groups.response(409, "Admin Not Found")
     @validate_schema(api_groups, GroupCreateSchema)
     def post(self, validated_schema):
-        pass
+        admin = db.Session.scalar(select(User).where(User.id == validated_schema["admin_id"]))
+
+        if not admin or admin.is_deleted:
+            api_groups.abort(409, "Admin Not Found")
+
+        group = Group(
+            name=validated_schema["name"],
+            description=validated_schema.get("description"),
+            admin=admin
+        )
+
+        db.Session.add(group)
+        db.Session.commit()
+
+        return api_groups.marshal(group, group_created), 201
 
     def get(self):
         pass
--- app/api/groups/marshmellow_schemas.py
+++ app/api/groups/marshmellow_schemas.py
@@ -2,6 +2,6 @@ from marshmallow import Schema, fields, validate
 
 
 class GroupCreateSchema(Schema):
-    admin_id = fields.Integer(validate=validate.Range(min=1))
+    admin_id = fields.Integer(required=True, validate=validate.Range(min=1))
     name = fields.Str(required=True, validate=validate.Length(1))
     description = fields.Str(validate=validate.Length(1))
