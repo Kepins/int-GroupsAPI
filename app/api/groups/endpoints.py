3bd3878f25f6d84152dd8e47af749e174f0ab073
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-25 14:52:00 +0200

    GRP-106: Models and response codes for create

--- app/api/groups/endpoints.py
+++ app/api/groups/endpoints.py
@@ -1,11 +1,21 @@
 from flask_restx import Resource
+from sqlalchemy import select
 
+from app import db
+from app.api.groups.marshmellow_schemas import GroupCreateSchema
 from app.api.groups.namespace import api_groups
+from app.api.groups.restx_models import group_create, group_created
+from app.validation import validate_schema
+from models import User
 
 
 @api_groups.route("/")
 class Groups(Resource):
-    def post(self):
+    @api_groups.expect(group_create)
+    @api_groups.response(201, "Success", group_created)
+    @api_groups.response(409, "Admin Not Found")
+    @validate_schema(api_groups, GroupCreateSchema)
+    def post(self, validated_schema):
         pass
 
     def get(self):
---
+++ app/api/groups/marshmellow_schemas.py
@@ -0,0 +1,7 @@
+from marshmallow import Schema, fields, validate
+
+
+class GroupCreateSchema(Schema):
+    admin_id = fields.Integer(validate=validate.Range(min=1))
+    name = fields.Str(required=True, validate=validate.Length(1))
+    description = fields.Str(validate=validate.Length(1))
