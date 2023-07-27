859433c52aa42e2d7c3bb2212347deef95f9c38d
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-27 13:13:05 +0200

    GRP-107: Quicker query for user existance

--- app/validation/existance.py
+++ app/validation/existance.py
@@ -1,11 +1,10 @@
-from sqlalchemy import select
+from sqlalchemy import exists, and_, not_
 
 from app import db
 from models import User
 
 
 def check_user_exists(id):
-    user = db.Session.scalar(select(User).where(User.id == id))
-    if not user or user.is_deleted:
-        return False
-    return True
+    return db.Session.query(
+        exists(User).where(and_(User.id == id, not_(User.is_deleted)))
+    ).scalar()
